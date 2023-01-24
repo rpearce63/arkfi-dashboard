0b9b1ccbaaf956f7612b03dea2a9e1d16ff41341
Glitch (glitch-hello-react) <none>
2023-01-24 18:06:43 +0000

    🚜😻 Checkpoint ./src/styles/styles.css:48120569/981 ./src/app.jsx:48120569/301

--- src/app.jsx
+++ src/app.jsx
@@ -40,14 +40,7 @@ export default function Home() {
           <span className="divider">|</span>
           <Link href="/about">About</Link>
         </div>
-        <a
-          className="btn--remix"
-          target="_top"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
-        >
-          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-          Remix on Glitch
-        </a>
+        
       </footer>
     </Router>
   );
