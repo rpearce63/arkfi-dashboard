c581424c802162b005da10fcd94210079a96d58a
Glitch (glitch-hello-react) <none>
2023-01-25 16:05:42 +0000

    🍯🍤 Checkpoint ./src/components/AddWallet.jsx:48120569/1071

--- src/components/AddWallet.jsx
+++ src/components/AddWallet.jsx
@@ -1,11 +1,22 @@
-import * as React from "react";
+import React, {useState} from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
 
 export default () => {
+  
+  const [address, setAddress] = useState();
+  
   return (
-    <Box>
-      <TextField id="add-wallet" label="Enter address" />
+    <Box sx={{backgroundColor: "lightgrey", padding: "10px "}}>
+      <TextField
+        id="add-wallet"
+        label="Enter address"
+        variant="outlined"
+        sx={{ backgroundColor: "white", marginRight: "1em" }}
+        onChange={e => setAddress(e.target.value)}
+      />
+      <Button variant="contained">Contained</Button>
     </Box>
   );
 };
