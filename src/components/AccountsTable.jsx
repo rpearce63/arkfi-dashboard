513d08ab7bbde2c3839c417a803f1f1ae8361d4b
Glitch (glitch-hello-react) <none>
2023-01-25 22:19:43 +0000

    😚🌂 Checkpoint ./src/components/AccountsTable.jsx:48120569/7729

--- src/components/AccountsTable.jsx
+++ src/components/AccountsTable.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -8,27 +8,32 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-
-export default function BasicTable({accounts}) {
-  
+export default function BasicTable({ accounts }) {
   const [totals, setTotals] = useState([]);
-  const [totalBalance, setTotalBalance] = useState(0);
-  
+
   useEffect(() => {
-    const balanceTotal = accounts.reduce((total, account) => total + parseFloat(account.principalBalance), 0);
-    setTotalBalance(balanceTotal)
-    setTotals([...totals, {balanceTotal}])
-  },[accounts])
+    const balanceTotal = accounts.reduce(
+      (total, account) => total + parseFloat(account.principalBalance),
+      0
+    );
+    const walletTotal = accounts.reduce((total, account) => total + parseFloat(account.walletBalance), 0);
+    const busdTotal = accounts.reduce((total, account) => total + parseFloat(account.busdBalance), 0);
+    const availTotal = accounts.reduce((total, account) => total + parseFloat(account.availableRewards), 0);
+    const depositsTotal = accounts.reduce((total, account) => total + parseFloat(account.deposits), 0);
+    
+    
+    setTotals({ ...totals, balanceTotal, walletTotal, busdTotal, availTotal, depositsTotal });
+  }, [accounts]);
 
   return (
     <TableContainer component={Paper} sx={{ marginTop: "2em" }}>
-      <Table sx={{ minWidth: 650, backgroundColor: "AliceBlue" }} aria-label="simple table">
+      <Table
+        sx={{ minWidth: 650, backgroundColor: "AliceBlue" }}
+        aria-label="simple table"
+      >
         <TableHeader></TableHeader>
         <TableBody>
-          <TableRow sx={{backgroundColor: "WhiteSmoke"}}>
-            <TableCell>{accounts.length}</TableCell>
-            <TableCell align="right">{totalBalance}</TableCell>
-          </TableRow>
+          <TotalsHeader accounts={accounts} totals={totals} />
           {accounts.map((row) => (
             <TableRow
               key={row.account}
@@ -56,24 +61,35 @@ export default function BasicTable({accounts}) {
 
 const TableHeader = () => {
   return (
-  <TableHead>
-          <TableRow>
-            <TableCell>Account</TableCell>
-            <TableCell align="right">Balance</TableCell>
-            <TableCell align="right">Wallet Balance</TableCell>
-            <TableCell align="right">BUSD</TableCell>
-            <TableCell align="right">Available</TableCell>
-            <TableCell align="right">CWR</TableCell>
-            <TableCell align="right">Daily ROI</TableCell>
-            <TableCell align="right">Total Deposits</TableCell>
-            <TableCell align="right">NDV</TableCell>
+    <TableHead>
+      <TableRow>
+        <TableCell>Account</TableCell>
+        <TableCell align="right">Balance</TableCell>
+        <TableCell align="right">Wallet Balance</TableCell>
+        <TableCell align="right">BUSD</TableCell>
+        <TableCell align="right">Available</TableCell>
+        <TableCell align="right">CWR</TableCell>
+        <TableCell align="right">Daily ROI</TableCell>
+        <TableCell align="right">Total Deposits</TableCell>
+        <TableCell align="right">NDV</TableCell>
 
-            <TableCell align="right">Max Payout</TableCell>
-          </TableRow>
-        </TableHead>
-  )
-}
+        <TableCell align="right">Max Payout</TableCell>
+      </TableRow>
+    </TableHead>
+  );
+};
 
-const TotalsHeader = () => {
-  
-}
+const TotalsHeader = ({accounts, totals}) => {
+  return (
+    <TableRow sx={{ backgroundColor: "WhiteSmoke" }}>
+      <TableCell>{accounts.length}</TableCell>
+      <TableCell align="right">{totals.balanceTotal}</TableCell>
+      <TableCell align="right">{totals.walletTotal}</TableCell>
+      <TableCell align="right">{totals.busdTotal}</TableCell>
+      <TableCell align="right">{totals.availTotal}</TableCell>
+      <TableCell></TableCell>
+      <TableCell></TableCell>
+      <TableCell align="right">{totals.depositsTotal}</TableCell>
+    </TableRow>
+  );
+};
