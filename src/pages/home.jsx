b18768cf47c2c5a411d02b40ed1fa5ea16527728
Glitch (glitch-hello-react) <none>
2023-01-25 17:16:52 +0000

    🌂🍟 Checkpoint ./src/api/arkfi.js:48120569/63 ./src/pages/home.jsx:48120569/2442

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,45 +6,42 @@ import AddWallet from "../components/AddWallet";
 import { initData } from "../api/arkfi";
 
 export default () => {
-  
   const accounts = [
-  "0x1ff661243cb97384102a69a466c887b4cC12d72a",
-  "0xb066550524e791c41672178975febb4e7038a3f8",
-];
-  
-  
+    "0x1ff661243cb97384102a69a466c887b4cC12d72a",
+    "0xb066550524e791c41672178975febb4e7038a3f8",
+  ];
+
   const [acctData, setAcctData] = useState([]);
-  const [addresses, setAddresses] = useState([])
-  
+  const [addresses, setAddresses] = useState([]);
+
   const getInitData = async () => {
-    console.log('getting data: ', addresses)
+    console.log("getting data: ", addresses);
     const accountInfo = await initData(addresses);
 
     setAcctData([...accountInfo]);
   };
 
   useEffect(() => {
-    const savedData = JSON.parse(localStorage.getItem('arkFiWallets'));
-    savedData && setAddresses([...savedData])
+    const savedData = JSON.parse(localStorage.getItem("arkFiWallets"));
+    savedData && setAddresses([...savedData]);
     setInterval(() => {
       getInitData();
     }, 60000);
-  }, [])
-  
+  }, []);
+
   useEffect(() => {
     getInitData();
-    localStorage.setItem('arkFiWallets', JSON.stringify(addresses))
+    localStorage.setItem("arkFiWallets", JSON.stringify(addresses));
   }, [addresses]);
-  
+
   const addWallet = (address) => {
-    setAddresses([...new Set([...addresses, address])])
-    
-  }
-  
+    setAddresses([...new Set([...addresses, address])]);
+  };
+
   return (
     <div>
-      <AddWallet addWallet={addWallet}/>
-      <AccountsTable accounts={acctData}/>
+      <AddWallet addWallet={addWallet} />
+      <AccountsTable accounts={acctData} />
     </div>
   );
 };
