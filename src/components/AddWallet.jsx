061a423aede3810292cade910a1b2b61c95d0aa8
Glitch (glitch-hello-react) <none>
2023-01-26 19:28:20 +0000

    ⏳🎾 Checkpoint ./src/components/AddWallet.jsx:48120569/5780

--- src/components/AddWallet.jsx
+++ src/components/AddWallet.jsx
@@ -1,31 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
-export default ({addWallet}) => {
-  
-  const [address, setAddress] = useState('');
+export default ({ addWallet }) => {
+  const [address, setAddress] = useState("");
   const processAddress = () => {
-    
-    !!address && addWallet(address)
-    setAddress('')
-  }
+    !!address && addWallet(address);
+    setAddress("");
+  };
   
+  const loadFile = () => {
+    
+  }
+
   return (
     <>
-    <Box sx={{backgroundColor: "lightgrey", padding: "10px ", borderRadius: "5px"}}>
-      <TextField
-        size="small"
-        id="add-wallet"
-        label="Enter address"
-        variant="outlined"
-        sx={{ backgroundColor: "white", marginRight: "1em", width: "55ch", borderRadius: "5px" }}
-        fullWidth
-        value={address}
-        onChange={e => setAddress(e.target.value)}
-      />
-      <Button variant="contained" onClick={processAddress}>Add Wallet</Button>
-   </Box> </>
+      <Box
+        sx={{
+          backgroundColor: "lightgrey",
+          padding: "10px ",
+          borderRadius: "5px",
+        }}
+      >
+        <Stack spacing={2} direction="row">
+          <TextField
+            size="small"
+            id="add-wallet"
+            label="Enter address"
+            variant="outlined"
+            sx={{
+              backgroundColor: "white",
+              marginRight: "1em",
+              width: "55ch",
+              borderRadius: "5px",
+            }}
+            fullWidth
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <Button variant="contained" onClick={processAddress}>
+            Add Wallet
+          </Button>
+          <Button variant="contained">Load Backup</Button>
+        </Stack>
+      </Box>{" "}
+    </>
   );
 };
