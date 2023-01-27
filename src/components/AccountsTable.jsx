2b75799eda40a5371f5843e57fc941b252aa8137
Glitch (glitch-hello-react) <none>
2023-01-27 17:18:11 +0000

    🌕👢 Checkpoint ./src/components/AccountsTable.jsx:48120569/18294 ./src/styles/styles.css:48120569/4

--- src/components/AccountsTable.jsx
+++ src/components/AccountsTable.jsx
@@ -9,13 +9,12 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import ToggleButton from "@mui/material/ToggleButton";
 import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
-import Switch from '@mui/material/Switch';
-import {GetArkPrice_Swap, backupData} from '../api/arkfi';
-import ConfirmationDialog from './ConfirmationDialog';
-import Button from '@mui/material/Button';
-import Controls from './Controls';
-import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
-
+import Switch from "@mui/material/Switch";
+import { GetArkPrice_Swap, backupData } from "../api/arkfi";
+import ConfirmationDialog from "./ConfirmationDialog";
+import Button from "@mui/material/Button";
+import Controls from "./Controls";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 
 function Timer(toDate) {
   var dateEntered = toDate + 86400000;
@@ -49,12 +48,12 @@ export default function AccountsTable({ accounts }) {
   const [timers, setTimers] = useState([]);
   const [selected, setSelected] = React.useState(false);
   const [isBusd, setIsBusd] = useState(false);
-  const [arkPrice, setArkPrice] = useState(0)
+  const [arkPrice, setArkPrice] = useState(0);
   //const [wallets, setWallets] = useState([]);
   //const [confirm, setConfirm] = useState(false);
   const [openDialog, setOpenDialog] = useState(false);
-  const [selectedRow, setSelectedRow] = useState('');
-  
+  const [selectedRow, setSelectedRow] = useState("");
+
   const updateTimers = () => {
     let _timers = {};
     for (const account of accounts) {
@@ -64,16 +63,15 @@ export default function AccountsTable({ accounts }) {
 
     setTimers(_timers);
   };
-  
+
   useEffect(() => {
     const getArkPrice = async () => {
       //setWallets(() => [...accounts]);
       const arkPrice = await GetArkPrice_Swap();
       setArkPrice(arkPrice);
-    }
+    };
     getArkPrice();
-    
-  }, [])
+  }, []);
 
   useEffect(() => {
     const balanceTotal = accounts.reduce(
@@ -117,88 +115,112 @@ export default function AccountsTable({ accounts }) {
       depositsTotal,
       maxPayoutTotal,
       nftRewardsTotal,
-      airdropsReceivedTotal
+      airdropsReceivedTotal,
     });
 
     const timerInterval = setInterval(() => {
       updateTimers();
       return () => clearInterval(timerInterval);
     }, 1000);
-    
-    
   }, [accounts]);
-  
-  const displayValue = (amount) => isBusd ? '$' + parseFloat(amount * arkPrice).toFixed(2) : parseFloat(amount).toFixed(2);
-  
+
+  const displayValue = (amount) =>
+    isBusd
+      ? "$" + parseFloat(amount * arkPrice).toFixed(2)
+      : parseFloat(amount).toFixed(2);
+
   const handleResponse = (response) => {
     setOpenDialog(false);
     removeWallet(response);
-  }
-  
+  };
+
   const openConfirmationDialog = (account) => {
     setOpenDialog(true);
     setSelectedRow(account);
-  }
-  
+  };
+
   const removeWallet = (isConfirmed) => {
     setOpenDialog(false);
     if (!isConfirmed) {
       return false;
     }
-    const stored = JSON.parse(localStorage.getItem('arkFiWallets'))
-    const updated = stored.filter(w => w !== selectedRow);
-    localStorage.setItem('arkFiWallets', JSON.stringify(updated));
+    const stored = JSON.parse(localStorage.getItem("arkFiWallets"));
+    const updated = stored.filter((w) => w !== selectedRow);
+    localStorage.setItem("arkFiWallets", JSON.stringify(updated));
     //const updatedWallets = accounts.filter(w => w.account !== account);
     //setWallets(() => [...updatedWallets]);
     window.location.reload(false);
-  }
-  
-  const formatAddress = address => `${address.substring(0,5)}...${address.slice(-5)}`
-  
+  };
+
+  const formatAddress = (address) =>
+    `${address.substring(0, 5)}...${address.slice(-5)}`;
+
   return (
     <>
-    <TableContainer component={Paper} sx={{ marginTop: "2em" }}>
-      
-      <Controls handleSwitch={() => setIsBusd(!isBusd)} backupData={backupData}/>
-      <Table
-        sx={{ minWidth: 650, backgroundColor: "AliceBlue" }}
-        aria-label="simple table"
-      >
-        
-        
-        <TableHeader ></TableHeader>
-        <TableBody>
-          <TotalsHeader accounts={accounts} totals={totals} displayValue={displayValue} />
-          {accounts.map((row) => (
-            <TableRow
-              key={row.account}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell className="remove-row" onClick={() => openConfirmationDialog(row.account)}>
-              <RemoveCircleOutlineIcon  sx={{fontSize: "14px"}}/>
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {formatAddress(row.account)}
-              </TableCell>
-              
-              <TableCell align="right">{Timer(row.lastAction)}</TableCell>
-              <TableCell align="right">{displayValue(row.principalBalance)}</TableCell>
-              <TableCell align="right">{displayValue(row.walletBalance)}</TableCell>
-              <TableCell align="right">{row.busdBalance}</TableCell>
-              <TableCell align="right">{displayValue(row.availableRewards)}</TableCell>
-              <TableCell align="right">{row.maxCwr}</TableCell>
-              <TableCell align="right">{row.roi}%</TableCell>
-              <TableCell align="right">{displayValue(row.deposits)}</TableCell>
-              <TableCell align="right">{row.ndv}</TableCell>
-              <TableCell align="right">{displayValue(row.maxPayout)}</TableCell>
-              <TableCell align="right">{displayValue(row.nftRewards)}</TableCell>
-              <TableCell align="right">{displayValue(row.airdropsReceived)}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-      {openDialog && <ConfirmationDialog handleResponse={handleResponse}/>}
+      <TableContainer component={Paper} sx={{ marginTop: "2em" }}>
+        <Controls
+          handleSwitch={() => setIsBusd(!isBusd)}
+          backupData={backupData}
+        />
+        <Table
+          sx={{ minWidth: 650, backgroundColor: "AliceBlue" }}
+          aria-label="simple table"
+        >
+          <TableHeader></TableHeader>
+          <TableBody>
+            <TotalsHeader
+              accounts={accounts}
+              totals={totals}
+              displayValue={displayValue}
+            />
+            {accounts.map((row) => (
+              <TableRow
+                key={row.account}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>
+                  <RemoveCircleOutlineIcon
+                    className="remove-row"
+                    onClick={() => openConfirmationDialog(row.account)}
+                    sx={{ fontSize: "14px" }}
+                  />
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {formatAddress(row.account)}
+                </TableCell>
+
+                <TableCell align="right">{Timer(row.lastAction)}</TableCell>
+                <TableCell align="right">
+                  {displayValue(row.principalBalance)}
+                </TableCell>
+                <TableCell align="right">
+                  {displayValue(row.walletBalance)}
+                </TableCell>
+                <TableCell align="right">{row.busdBalance}</TableCell>
+                <TableCell align="right">
+                  {displayValue(row.availableRewards)}
+                </TableCell>
+                <TableCell align="right">{row.maxCwr}</TableCell>
+                <TableCell align="right">{row.roi}%</TableCell>
+                <TableCell align="right">
+                  {displayValue(row.deposits)}
+                </TableCell>
+                <TableCell align="right">{row.ndv}</TableCell>
+                <TableCell align="right">
+                  {displayValue(row.maxPayout)}
+                </TableCell>
+                <TableCell align="right">
+                  {displayValue(row.nftRewards)}
+                </TableCell>
+                <TableCell align="right">
+                  {displayValue(row.airdropsReceived)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {openDialog && <ConfirmationDialog handleResponse={handleResponse} />}
     </>
   );
 }
@@ -242,8 +264,12 @@ const TotalsHeader = ({ accounts, totals, displayValue }) => {
       <TableCell align="right">{displayValue(totals.depositsTotal)}</TableCell>
       <TableCell></TableCell>
       <TableCell align="right">{displayValue(totals.maxPayoutTotal)}</TableCell>
-      <TableCell align="right">{displayValue(totals.nftRewardsTotal)}</TableCell>
-      <TableCell align="right">{displayValue(totals.airdropsReceivedTotal)}</TableCell>
+      <TableCell align="right">
+        {displayValue(totals.nftRewardsTotal)}
+      </TableCell>
+      <TableCell align="right">
+        {displayValue(totals.airdropsReceivedTotal)}
+      </TableCell>
     </TableRow>
   );
 };
