d8c89f7255e1bd64e3b0f664596de4a62455c382
Glitch (glitch-hello-react) <none>
2023-01-31 23:23:55 +0000

    🏝🚜 Checkpoint ./src/components/Header.jsx:48120569/328

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,7 @@ import { getArkPrice } from "../api/utils";
 
 const Header = () => {
   const [arkPrice, setArkPrice] = useState(0);
-  const [displayPrice, setDisplayPrice] = useState(0);
-
+  
   const getUpdatedArkPrice = async () => {
     const price = await getArkPrice();
     setArkPrice(price);
@@ -17,20 +16,10 @@ const Header = () => {
     return () => clearInterval(priceInterval);
   }, []);
 
-  useEffect(() => {
-    const increment = setInterval(() => {
-      console.log('arkPrice: ', arkPrice)
-      setDisplayPrice((prev) => {
-         return prev + 0.01;
-      });
-      return () => clearInterval(increment);
-    }, 1000);
-  }, [arkPrice]);
-
   return (
     <header className="App-header">
       <h1 className="page-title">ArkFi Multi-Wallet Dashboard</h1>
-      <h2 className="page-title">Ark Price: ${displayPrice}</h2>
+      <h2 className="page-title">Ark Price: ${arkPrice}</h2>
     </header>
   );
 };
