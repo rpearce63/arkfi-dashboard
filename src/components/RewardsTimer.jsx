da7822f64a111df08f9b28c76940f9079f7a2a9b
Glitch (glitch-hello-react) <none>
2023-02-01 14:32:21 +0000

    🗼🍤 Checkpoint ./src/components/RewardsTimer.jsx:48120569/2229

--- src/components/RewardsTimer.jsx
+++ src/components/RewardsTimer.jsx
@@ -1,45 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-export default ({toDate}) => {
-  
-  const [time, setTime] = useState('');
-  
-  const updateTimer = () =>{
-  var dateEntered = toDate + 86400000;
-  //console.log('date: ', toDate, dateEntered)
-  var now = new Date();
-  var difference = dateEntered - now.getTime();
+export default ({ toDate }) => {
+  const [time, setTime] = useState("");
 
-  if (difference <= 0) {
-    //$("#vaulttimer").text("00:00:00");
-    //clearInterval(rewardsTimer);
-    const haltTimer = true;
-    setTime("00:00:00");
-  } else {
-    var seconds = Math.floor(difference / 1000);
-    var minutes = Math.floor(seconds / 60);
-    var hours = Math.floor(minutes / 60);
-    var days = Math.floor(hours / 24);
+  const updateTimer = () => {
+    var dateEntered = toDate + 86400000;
+    var now = new Date();
+    var difference = dateEntered - now.getTime();
 
-    hours %= 24;
-    minutes %= 60;
-    seconds %= 60;
+    if (difference <= 0) {
+      setTime("00:00:00");
+    } else {
+      var seconds = Math.floor(difference / 1000);
+      var minutes = Math.floor(seconds / 60);
+      var hours = Math.floor(minutes / 60);
+      var days = Math.floor(hours / 24);
 
-    const hourText = hours < 10 ? "0" + hours : hours;
-    const minutesText = minutes < 10 ? "0" + minutes : minutes;
-    const secondsText = seconds < 10 ? "0" + seconds : seconds;
-    setTime(hourText + ":" + minutesText + ":" + secondsText);
-  }}
-  
+      hours %= 24;
+      minutes %= 60;
+      seconds %= 60;
+
+      const hourText = hours < 10 ? "0" + hours : hours;
+      const minutesText = minutes < 10 ? "0" + minutes : minutes;
+      const secondsText = seconds < 10 ? "0" + seconds : seconds;
+      setTime(hourText + ":" + minutesText + ":" + secondsText);
+    }
+  };
+
   useEffect(() => {
     updateTimer();
     const interval = setInterval(() => {
       updateTimer();
       return () => clearInterval(interval);
-    },1000)
-  },[])
-  
-  
-  return <span>{time}</span>;
+    }, 1000);
+  }, []);
 
-}
+  return <span>{time}</span>;
+};
