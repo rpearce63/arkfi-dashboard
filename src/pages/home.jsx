94673ff457b2f602bae8d4440df6d7c3d7481ff3
Glitch (glitch-hello-react) <none>
2023-03-20 16:40:25 +0000

    💁👰 Checkpoint ./src/pages/home.jsx:48120569/125

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,16 +6,13 @@ import AddWallet from "../components/AddWallet";
 import { initData } from "../api/arkfi";
 
 export default () => {
-  const accounts = [
-    "0x1ff661243cb97384102a69a466c887b4cC12d72a",
-    "0xb066550524e791c41672178975febb4e7038a3f8",
-  ];
+  
 
   const [acctData, setAcctData] = useState([]);
 
   const getInitData = async () => {
     const savedData = [...new Set(JSON.parse(localStorage.getItem("arkFiWallets")))] || [];
-    localStorage.setItem('arkFiWallets', JSON.stringify([...new Set([...savedData])]))
+    //localStorage.setItem('arkFiWallets', JSON.stringify([...new Set([...savedData])]))
     const accountInfo = await initData(savedData);
 
     accountInfo.length && setAcctData([...accountInfo]);
