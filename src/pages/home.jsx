a11ec98e00a5d4e3ec1af90df81a543564b1e903
Glitch (glitch-hello-react) <none>
2023-04-18 12:53:43 +0000

    🍏🎊 Checkpoint ./src/pages/home.jsx:48120569/263

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,15 +12,16 @@ export default () => {
 
   const getInitData = async () => {
     const savedData = [...new Set(JSON.parse(localStorage.getItem("arkFiWallets")))] || [];
+    
     localStorage.setItem('arkFiWallets', JSON.stringify([...new Set([...savedData])]))
-//     for(const account of savedData) {
-//    const accountInfo = await initData(account);
+    for(const account of savedData) {
+   const accountInfo = await initData([account]);
+   console.log(accountInfo)
+    accountInfo.length && setAcctData((prev) => [...prev, ...accountInfo[0]]);
+ }
+//     const accountInfo = await initData(savedData);
 
-//     accountInfo.length && setAcctData([...acctData, accountInfo[0]]);
-//  }
-    const accountInfo = await initData(savedData);
-
-    accountInfo.length && setAcctData([...accountInfo]);
+//     accountInfo.length && setAcctData([...accountInfo]);
   };
 
   useEffect(() => {
